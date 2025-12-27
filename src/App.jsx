a58995fa55df2f82fdf919a39fb6184c08ea5e53
OEM Configuration (temporary user) <gustavo.prog40@gmail.com>
2025-12-27 13:32:50 -0300

    localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import api from "./api";
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [produtos, setProdutos] = useState([]);
 
+  function setLocalstorageProfile(id) {
+    localStorage.setItem("MY-PROD", id);
+  }
+
+  async function getProdutos() {
+    try {
+      const { data } = await api.get("/get-products");
+
+      setProdutos(data);
+    } catch (error) {
+      return alert(error);
+    }
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    getProdutos();
+  }, []);
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {produtos.map((items) => {
+          return (
+            <button onClick={() => setLocalstorageProfile(items.id)}>
+              <div key={items.id}>
+                <p>{items.name}</p>
+                <img width="300" src={items.image} />
+                <p>Price: R$ {items.price}</p>
+              </div>
+            </button>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
